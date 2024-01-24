c90d5b14c69d82d966f40c3e193b0ec7d71b75e4
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-01-24 15:05:37 -0800

    refactor layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import logo from "@/public/NHIBIT_Av03-White.png";
 
 export default function Home() {
   return (
-    <main className={`${styles.main} flex flex_center`}>
-      <div className={`${styles.logo_wrapper} flex`}>
-        <Image src={logo} alt={"NHIBIT logo"} className={styles.image} />
-      </div>
+    <main className={`${styles.main} grid`}>
+      <section className={styles.container_1}></section>
+      <section className={`${styles.logo_container} flex`}>
+        <div className={`${styles.logo_wrapper} flex`}>
+          <Image src={logo} alt={"NHIBIT logo"} className={styles.image} />
+        </div>
+      </section>
     </main>
   );
 }
