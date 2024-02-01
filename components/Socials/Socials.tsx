57648448da103ce59bc313c261e7c4fafe3c28cf
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-02-01 10:20:40 -0800

    tighten socials layout

--- components/Socials/Socials.tsx
+++ components/Socials/Socials.tsx
@@ -3,41 +3,50 @@ import styles from "@/components/Socials/Socials.module.scss";
 import instagram from "@/public/social_icons/instagram-svgrepo-com.svg";
 import soundcloud from "@/public/social_icons/soundcloud-svgrepo-com.svg";
 import spotify from "@/public/social_icons/spotify-dark-svgrepo-com.svg";
-import youtube from "@/public/social_icons/youtube-svgrepo-com.svg";
+// import youtube from "@/public/social_icons/youtube-svgrepo-com.svg";
 
 function Socials() {
-  const height = "50";
-  const width = "50";
+  const height = "35";
+  const width = "35";
   return (
     <div className={`${styles.socials_wrapper} flex`}>
-      <Image
-        height={height}
-        width={width}
-        src={spotify}
-        alt={"spotify icon"}
-        className={styles.image}
-      />
-      <Image
-        height={height}
-        width={width}
-        src={soundcloud}
-        alt={"soundcloud icon"}
-        className={styles.image}
-      />
-      <Image
-        height={height}
-        width={width}
-        src={instagram}
-        alt={"instagram icon"}
-        className={styles.image}
-      />
-      <Image
+      <a
+        href="https://open.spotify.com/artist/71tcBzi4vKYIULaCMuWDbe?si=ewg7sQ7STeGl_oNZ7T9kuw"
+        className={styles.a}
+      >
+        <Image
+          height={height}
+          width={width}
+          src={spotify}
+          alt={"spotify icon"}
+          className={styles.image}
+        />
+      </a>
+      <a href="https://soundcloud.com/nhibit" className={styles.a}>
+        <Image
+          height={55}
+          width={70}
+          src={soundcloud}
+          alt={"soundcloud icon"}
+          className={`${styles.image} ${styles.soundcloud_icon}`}
+        />
+      </a>
+      <a href="https://instagram.com/nhibit" className={styles.a}>
+        <Image
+          height={height}
+          width={width}
+          src={instagram}
+          alt={"instagram icon"}
+          className={styles.image}
+        />
+      </a>
+      {/* <Image
         height={height}
         width={width}
         src={youtube}
         alt={"youtube icon"}
         className={styles.image}
-      />
+      /> */}
     </div>
   );
 }
