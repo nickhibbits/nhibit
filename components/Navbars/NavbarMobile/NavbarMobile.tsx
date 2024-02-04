e11a39e9c4dcfdd3fc9d105d2be82538ba60f40f
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-02-04 12:35:42 -0800

    mobile nav animation looking good

--- components/Navbars/NavbarMobile/NavbarMobile.tsx
+++ components/Navbars/NavbarMobile/NavbarMobile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { navOptions } from "@/components/Navbars/navOptions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import navbarMobileStyles from "@/components/Navbars/NavbarMobile/NavbarMobile.module.scss";
 import layoutStyles from "@/styles/layout.module.scss";
@@ -13,41 +13,46 @@ import Hamburger from "@/components/Hamburger/Hamburger";
 function NavbarMobile() {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <nav className={navbarMobileStyles.nav}>
         <div
           className={`${navbarMobileStyles.svgWrapper_menu} ${layoutStyles.padding_wrapper}`}
-          onClick={() => setMenuOpen(!menuOpen)}
+          onClick={() => handleMenuOpen()}
         >
           <Hamburger />
         </div>
       </nav>
 
-      {menuOpen && (
-        <div className={navbarMobileStyles.options_container}>
-          <div className={navbarMobileStyles.option_socials_wrapper}>
-            <ul className={navbarMobileStyles.options_wrapper}>
-              {navOptions.map((option, i) => {
-                return (
-                  <li className={navbarMobileStyles.option} key={i}>
-                    <Link
-                      href={option.toLowerCase()}
-                      className={`${navbarMobileStyles.anchor} flex`}
-                    >
-                      <p className={navbarMobileStyles.num}>{i + 1}.</p>
-                      <p className={navbarMobileStyles.option_text}>
-                        {option.toUpperCase()}
-                      </p>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-            <SocialsMobile />
-          </div>
+      <div
+        className={navbarMobileStyles.options_container}
+        data-visible={menuOpen ? "true" : "false"}
+      >
+        <div className={navbarMobileStyles.option_socials_wrapper}>
+          <ul className={navbarMobileStyles.options_wrapper}>
+            {navOptions.map((option, i) => {
+              return (
+                <li className={navbarMobileStyles.option} key={i}>
+                  <Link
+                    href={option.toLowerCase()}
+                    className={`${navbarMobileStyles.anchor} flex`}
+                  >
+                    <p className={navbarMobileStyles.num}>{i + 1}.</p>
+                    <p className={navbarMobileStyles.option_text}>
+                      {option.toUpperCase()}
+                    </p>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <SocialsMobile />
         </div>
-      )}
+      </div>
     </>
   );
 }
