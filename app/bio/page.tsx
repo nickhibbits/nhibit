08e1d5b8762eadce3030d3dc1caf850fdd090bbb
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-02-04 16:11:29 -0800

    add logo to mobile nav

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -1,6 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import headshot from "@/public/headshot.jpeg";
 
 export default function Home() {
-  return <main className={`${styles.main} flex `}>Bio</main>;
+  return (
+    <main className={`${styles.main} flex `}>
+      <div className={styles.image_wrapper}>
+        <Image className={styles.image} src={headshot} alt="DJ NHIBIT" />
+      </div>
+      <section>
+        <p className={styles.text}>
+          Drawing creative inspiration from his native west coast house scene
+          and the minimal tech sound out of the UK, NHIBIT combines tight
+          rhythms, driving basslines, and textures that creep and evolve to
+          produce his own signature style. Blending club heaters and emotive
+          arrangements into all his live mixes and original productions, he
+          keeps the energy up on the dance floor and his listeners' focus
+          locked. If you're seeking a fresh vibe with hypnotic grooves and lush
+          melodies, you're in the right place.
+        </p>
+      </section>
+    </main>
+  );
 }
--- components/Navbars/NavbarMobile/NavbarMobile.tsx
+++ components/Navbars/NavbarMobile/NavbarMobile.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { navOptions } from "@/components/Navbars/navOptions";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import navbarMobileStyles from "@/components/Navbars/NavbarMobile/NavbarMobile.module.scss";
 import layoutStyles from "@/styles/layout.module.scss";
+import logo from "@/public/NHIBIT_Av03-White.png";
 
 import Link from "next/link";
 import SocialsMobile from "@/components/Socials/SocialsMobile/SocialsMobile";
 import Hamburger from "@/components/Hamburger/Hamburger";
+import Image from "next/image";
 
 function NavbarMobile() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,6 +24,13 @@ function NavbarMobile() {
   return (
     <>
       <nav className={navbarMobileStyles.nav}>
+        <div className={`${navbarMobileStyles.logo_wrapper}`}>
+          <Image
+            src={logo}
+            alt={"NHIBIT logo"}
+            className={navbarMobileStyles.image}
+          />
+        </div>
         <div
           className={`${navbarMobileStyles.svgWrapper_menu} ${layoutStyles.padding_wrapper}`}
           onClick={() => handleMenuOpen()}
