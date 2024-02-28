66f5806dba9d99fe4ff9b0f8b717415f6ada8473
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-02-28 14:35:00 -0800

    Add message if shows length is 0

--- components/ShowsTable/ShowsTable.tsx
+++ components/ShowsTable/ShowsTable.tsx
@@ -1,38 +1,52 @@
 import ShowRow from "@/components/ShowsTable/ShowRow/ShowRow";
 
-const shows = [
-  {
-    city: "S Lake Tahoe",
-    where: "Heavenly Ski Resort @ East Peak",
-    when: "3/3/23",
-    start: "12pm",
-    end: "2pm",
-  },
-  {
-    city: "S Lake Tahoe",
-    where: "Heavenly Ski Resort @ East Peak",
-    when: "3/10/23",
-    start: "12pm",
-    end: "2pm",
-  },
+const shows: any[] = [
+  // {
+  //   city: "S Lake Tahoe",
+  //   where: "Heavenly Ski Resort @ East Peak",
+  //   when: "3/3/23",
+  //   start: "12pm",
+  //   end: "2pm",
+  // },
+  // {
+  //   city: "S Lake Tahoe",
+  //   where: "Heavenly Ski Resort @ East Peak",
+  //   when: "3/10/23",
+  //   start: "12pm",
+  //   end: "2pm",
+  // },
 ];
 
 function ShowsTable() {
-  return (
-    <ul>
-      {shows.map((show, i) => {
-        return (
-          <ShowRow
-            city={show.city}
-            where={show.where}
-            when={show.when}
-            start={show.start}
-            end={show.end}
-          />
-        );
-      })}
-    </ul>
-  );
+  if (shows.length === 0) {
+    return (
+      <p>
+        No shows scheduled. For bookings contact{" "}
+        <a
+          href="mailto:nhibitmusic@gmail.com"
+          style={{ textDecoration: "underline" }}
+        >
+          nhibitmusic@gmail.com
+        </a>
+      </p>
+    );
+  } else {
+    return (
+      <ul>
+        {shows.map((show, i) => {
+          return (
+            <ShowRow
+              city={show.city}
+              where={show.where}
+              when={show.when}
+              start={show.start}
+              end={show.end}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default ShowsTable;
